f843c1ee19e025c39e53f4ffee7203fcb54bd7a1
Emmanuel Hope Mensah <manuelhopemensah@gmail.com>
2023-03-20 10:54:24 -0500

    javascript file added

--- js/feb.js
+++ js/feb.js
@@ -1,9 +1,7 @@
 
-let answer = document.querySelector('.answer');
+
 const toggleSwitch = document.querySelector('.theme-slider input[type="checkbox"]');
-
-
-
+let answer = document.querySelector('.answer');
 
 
 
@@ -13,28 +11,21 @@ function switchTheme(e) {
     let background = document.querySelector('body');
     let background1 = document.querySelector('.box');
     let themebox = document.querySelector('#themebox');
-
-   
-  
+ 
     if (e.target.checked) { 
         background.style.background = 'white';
         background1.style.background = '#e5e5e5';
         themebox.style.background = '#e5e5e5';
         themebox.style.color = 'black';
         answer.style.color = 'black';
-        c1.style.color = 'red';
         background.style.transition = '500ms';
         background1.style.transition = '500ms';
         themebox.style.transition = '700ms';
         themebox.style.transition = '700ms';
-       
-        
-
-
     }
   
 
-    else {
+     else {
         background.style.background = '#1C1C1C';
         background1.style.background = 'black';
         themebox.style.background = 'black';
@@ -48,3 +39,33 @@ function switchTheme(e) {
 }
   
 toggleSwitch.addEventListener('change', switchTheme,false);
+
+
+let clear = document.querySelector('.ac');
+
+clear.addEventListener('click', function() {
+    answer.value = "";
+});
+
+function Key(keys) {
+    answer.value += keys;
+ };
+
+ function Answer() {
+    var equals = answer.value;
+    var equalsfinal = eval(equals);
+    answer.value = equalsfinal;
+ }
+
+ function Per(){
+    var equals = answer.value;
+    var finalle = equals/100;
+    answer.value = finalle;
+ }
+
+ function Pm(){
+    var equals = answer.value;
+    var finalle = equals * -1;
+    answer.value = finalle;
+ };
+
